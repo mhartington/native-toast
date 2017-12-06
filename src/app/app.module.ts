0ac967735140fb7596714fdf6569e731a7a39102
mhartington <mikehartington@gmail.com>
2017-12-06 10:35:47 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { Toast } from '@ionic-native/toast';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -22,6 +22,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+    Toast,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, Platform } from 'ionic-angular';
+import { Toast } from "@ionic-native/toast";
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public platform: Platform,
+    public toast: Toast,
+    public navCtrl: NavController) {
 
+  }
+
+  showToast(){
+    this.platform.ready()
+    .then(()=>{
+      this.toast.show(`I'm a toast`, '5000', 'center').subscribe(
+        res => console.log(res)
+      )
+    })
   }
 
 }
